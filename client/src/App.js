b91fa1a3ecb4c4d83f216dfea376be08754c7e11
HarshiRaul24 <harshitraul15@gmail.com>
2024-02-13 00:46:15 +0530

    Phase two

--- client/src/App.js
+++ client/src/App.js
@@ -10,8 +10,8 @@ function App() {
     <div className="App">
       {/* <Landing/> */}
       {/* <FAQ/> */}
-      {/* <Countdown/> */}
-      {/* <CollegeMessage/> */}
+      <Countdown/>
+      <CollegeMessage/>
       <Prize/>
     </div>
   );
