bfbf3e5e94530de79125f2f16f7134dd581ee7d7
HarshiRaul24 <harshitraul15@gmail.com>
2024-02-13 10:36:46 +0530

    Phase two

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,9 @@
 import './App.css';
-import CollegeMessage from './Components/CollegeMessage';
-import Prize from './Components/Prize';
-// import Countdown from './Components/Countdown';
-// import FAQ from './Components/FAQ';
-// import Landing from './Pages/Landing';
 
 function App() {
   return (
     <div className="App">
-      {/* <Landing/> */}
-      {/* <FAQ/> */}
-      <Countdown/>
-      <CollegeMessage/>
-      <Prize/>
+      
     </div>
   );
 }
