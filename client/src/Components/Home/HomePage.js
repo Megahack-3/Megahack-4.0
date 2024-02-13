85659c18e69278cf543942373a739371a40d35ca
HarshiRaul24 <harshitraul15@gmail.com>
2024-02-13 16:18:05 +0530

    Hompage update

--- client/src/Components/Home/HomePage.js
+++ client/src/Components/Home/HomePage.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import LOGOO from "../../images/MEGA HACK 4.0.png";
+import LOGO from "./../../assets/images/logo.png";
+import mh from "./../../assets/images/Text.png"
+import "./HomePage.css"
 
 function HomePage() {
   return (
-    <div className="container" style={{backgroundImage: `url("../MEGA HACK 4.0.png")`}}>
-      {/* <div className="logo"></div> */}
-      <img className="logo" src={LOGOO} alt="Logo" />
-      <div className="text">
-        <h1>CODE | COMPETE | CONQUER</h1>
+    <div className="Homepage">
+      <img className="logo-home" src={LOGO} alt="Logo" />
+      <img className='logo-text' src={mh} alt="" />
+      <div className="home-text">
+        <h2>CODE | COMPETE | CONQUER</h2>
       </div>
     </div>
   );
