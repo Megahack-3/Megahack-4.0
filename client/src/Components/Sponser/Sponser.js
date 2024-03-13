b3c7b30a525da37d59bdee6d733cea01c10a1335
devang1650 <devangvartak09@gmail.com>
2024-03-13 11:30:51 +0530

    Sponsers Gride Alignment changed

--- client/src/Components/Sponser/Sponser.js
+++ client/src/Components/Sponser/Sponser.js
@@ -4,7 +4,6 @@ import axure from "../../images/axure.png";
 import GDSC from "../../images/GDSC.png";
 import hacktoskill from "../../images/hacktoskill.png";
 
-
 const Sponsor = () => {
     const logos = [
         {
@@ -13,8 +12,8 @@ const Sponsor = () => {
             image: axure,
             webUrl: "https://www.axure.com/",
             header: "In Kind Partner",
-            height: 100, 
-            width: 200, 
+            height: 100,
+            width: 200,
         },
         {
             id: 2,
@@ -22,8 +21,8 @@ const Sponsor = () => {
             image: GDSC,
             webUrl: "https://gdsc.community.dev/st-john-college-of-engineering-management-palghar/",
             header: "Community partner",
-            height: 140, 
-            width: 150, 
+            height: 140,
+            width: 150,
         },
         {
             id: 3,
@@ -31,8 +30,8 @@ const Sponsor = () => {
             image: hacktoskill,
             webUrl: "https://hack2skill.com/",
             header: "Powered By",
-            height: 100, 
-            width: 175, 
+            height: 100,
+            width: 175,
         },
     ];
 
@@ -42,30 +41,43 @@ const Sponsor = () => {
         }
     };
 
-    return (
-
-        <>
-            <h1 id="Sponsors" style={{ textAlign: "center", fontWeight: "bold",fontSize:50 }}>Our Sponsors</h1>
+    const renderGrid = (logos, header) => (
+        <div>
+            <h2 style={{ textAlign: "center", fontWeight: "bold", fontSize: 30 }}>{header}</h2>
             <div className="sponsor-grid">
                 {logos.map((logo) => (
                     <div key={logo.id} className="sponsor-wrapper">
-                        <p style={{ textAlign: "center", fontWeight: "bold",fontSize:25 }}>{logo.header}</p>
+                        <p style={{ textAlign: "center", fontWeight: "bold", fontSize: 25 }}></p>
                         <div className="sponsor-container" style={{ textAlign: "center" }}>
                             <div onClick={() => openLink(logo.webUrl)}>
-                                <a href={logo.webUrl}/>
+                                <a href={logo.webUrl} />
                                 <img
                                     src={logo.image}
                                     alt={logo.name}
                                     className="sponsor"
-                                    style={{ height: logo.height, width: logo.width }} 
+                                    style={{ height: logo.height, width: logo.width }}
                                 />
                             </div>
                         </div>
                     </div>
                 ))}
             </div>
+        </div>
+    );
 
+  
+    const groupedLogos = logos.reduce((acc, logo) => {
+        if (!acc[logo.header]) {
+            acc[logo.header] = [];
+        }
+        acc[logo.header].push(logo);
+        return acc;
+    }, {});
 
+    return (
+        <>
+            <h1 id="Sponsors" style={{ textAlign: "center", fontWeight: "bold", fontSize: 50 }}>Our Sponsors</h1>
+            {Object.entries(groupedLogos).map(([header, logos]) => renderGrid(logos, header))}
         </>
     );
 };
